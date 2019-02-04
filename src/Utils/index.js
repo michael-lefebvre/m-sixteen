4cc2463682ed0e74664478cebf2eede5e4a5c50b
Michael Lefebvre <michael@scenedata.com>
2019-02-04 21:39:01 +0100

    Utils, quick clean

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -6,25 +6,9 @@ export { default as StoryTrigger } from "./StoryTrigger";
 export const AnimatedDiv = animated(Globals.defaultElement)
 export const raf = cb => Globals.requestFrame(cb)
 export const caf = cb => Globals.cancelFrame(cb)
+
 export const getParams = ({ match: { params } }) => params
 export const getSection = props => getParams(props).section
-export const isSectionActive = (props, section) => getSection(props) === section
-export const getReleaseStateFromProps = ({ id, prevId, wasActive, viewPort }, release) => {
-  const active = id === release;
-  const back = prevId === release;
-  const off = back && wasActive;
-  const show = !(!active && !back && !off)
-
-  return {
-     active,
-     off,
-     back,
-     show,
-     viewPort
-  }
-}
-export const hideReleaseStory = ({ active, back, off }) => !active && !back && !off
-export const isHome = (props) => !getSection(props)
 
 export const getImageUrl = path => getStaticUrl(`images/${path}`)
 export const getPhotoUrl = path => getStaticUrl(`photos/${path}`)
