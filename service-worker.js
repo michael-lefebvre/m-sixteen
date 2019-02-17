be1117b6a6a393f8932715a0a354dd42078c51ce
Michael Lefebvre <michael@scenedata.com>
2019-02-17 22:19:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/m-sixteen/precache-manifest.77a9012622d1c46545b193c4bf73da51.js"
+  "/m-sixteen/precache-manifest.a73ccbaa8930a32a39a1e766ad76d6c4.js"
 );
 
 workbox.clientsClaim();
