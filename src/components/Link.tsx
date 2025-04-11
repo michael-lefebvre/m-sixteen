66e42baa4cb9c3580abc5eba2ec6d9b7388811b4
Michael Lefebvre <michael@scenedata.com>
2025-04-12 01:54:54 +0200

    improve Link component with memo

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,4 +1,10 @@
-import { useCallback, useMemo, type AnchorHTMLAttributes, type DetailedHTMLProps } from 'react';
+import {
+  memo,
+  useCallback,
+  useMemo,
+  type AnchorHTMLAttributes,
+  type DetailedHTMLProps,
+} from 'react';
 
 import { useIsActivePath } from '@/hooks/useIsActivePath';
 import { push } from '@/utils/navigation';
@@ -15,57 +21,51 @@ type LinkProps = Omit<AnchorProps, 'className' | 'style' | 'children'> & {
   exact?: boolean;
 };
 
-export const Link = ({
-  className,
-  style,
-  onClick: originalOnClick,
-  href,
-  children,
-  exact,
-  ...props
-}: LinkProps) => {
-  const realHref = useMemo(() => href, [href]);
-  const isActive = useIsActivePath(realHref, exact);
+export const Link = memo(
+  ({ className, style, onClick: originalOnClick, href, children, exact, ...props }: LinkProps) => {
+    const realHref = useMemo(() => href, [href]);
+    const isActive = useIsActivePath(realHref, exact);
 
-  const config = useMemo<Config>(() => ({ active: isActive }), [isActive]);
+    const config = useMemo<Config>(() => ({ active: isActive }), [isActive]);
 
-  const onClick = useCallback(
-    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      const currentTarget = event.currentTarget;
-      const url = new URL(event.currentTarget.href, window.location.origin);
-      const currentLocation = new URL(window.location.href, window.location.origin);
-      const hasDifferentOrigin = url.origin !== currentLocation.origin;
-      const hasModifier = currentTarget.download != '' || currentTarget.target != '';
-      const hasMetaKey = event.metaKey || event.ctrlKey;
-      const isSamePage = url.pathname === currentLocation.pathname;
-      if (isSamePage) {
-        event.preventDefault();
-        return;
-      }
+    const onClick = useCallback(
+      (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        const currentTarget = event.currentTarget;
+        const url = new URL(event.currentTarget.href, window.location.origin);
+        const currentLocation = new URL(window.location.href, window.location.origin);
+        const hasDifferentOrigin = url.origin !== currentLocation.origin;
+        const hasModifier = currentTarget.download != '' || currentTarget.target != '';
+        const hasMetaKey = event.metaKey || event.ctrlKey;
+        const isSamePage = url.pathname === currentLocation.pathname;
+        if (isSamePage) {
+          event.preventDefault();
+          return;
+        }
 
-      const shouldRunDefaultBehavior = hasDifferentOrigin || hasModifier || hasMetaKey;
+        const shouldRunDefaultBehavior = hasDifferentOrigin || hasModifier || hasMetaKey;
 
-      if (originalOnClick != undefined) {
-        originalOnClick(event);
-      }
+        if (originalOnClick != undefined) {
+          originalOnClick(event);
+        }
 
-      if (!shouldRunDefaultBehavior) {
-        event.preventDefault();
-        push(url.pathname);
-      }
-    },
-    [originalOnClick],
-  );
+        if (!shouldRunDefaultBehavior) {
+          event.preventDefault();
+          push(url.pathname);
+        }
+      },
+      [originalOnClick],
+    );
 
-  return (
-    <a
-      {...props}
-      style={typeof style === 'function' ? style(config) : style}
-      className={typeof className === 'function' ? className(config) : className}
-      onClick={onClick}
-      href={realHref}
-    >
-      {typeof children === 'function' ? children(config) : children}
-    </a>
-  );
-};
+    return (
+      <a
+        {...props}
+        style={typeof style === 'function' ? style(config) : style}
+        className={typeof className === 'function' ? className(config) : className}
+        onClick={onClick}
+        href={realHref}
+      >
+        {typeof children === 'function' ? children(config) : children}
+      </a>
+    );
+  },
+);
